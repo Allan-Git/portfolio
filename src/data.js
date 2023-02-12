c3a29adeaf0f01485d20f8db932aac0a88ec4c2a
Allan <sale_allan@hotmail.fr>
2023-02-12 16:26:32 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const products = [
     {
       id: 1,
-      img: "https://i.ibb.co/H2sQnMy/shop.png",
+      img: "https://i.ibb.co/r605mPV/screencapture-localhost-3000-2023-02-12-16-25-10.png",
       link: "http://allan.dev",
     },
     {
